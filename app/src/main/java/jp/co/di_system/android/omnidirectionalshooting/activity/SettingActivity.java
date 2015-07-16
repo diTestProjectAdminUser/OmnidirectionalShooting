d1a3d036367267178725f2f61321118853ccd0aa
omnidirectionalShooting01 <omnidirectionalShooting01@gmail.com>
2015-07-16 22:03:07 +0900

    omnidirectionalShooting01 BaseActivityを継承して生成するように修正 ボタン実装と端末内に「攻撃音」と「BGM」の設定を保存するように実装

--- app/src/main/java/jp/co/di_system/android/omnidirectionalshooting/activity/SettingActivity.java
+++ app/src/main/java/jp/co/di_system/android/omnidirectionalshooting/activity/SettingActivity.java
@@ -9,29 +9,48 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.util.Log;
-
+import android.content.SharedPreferences;
+import android.content.Context;
 import jp.co.di_system.android.omnidirectionalshooting.R;
 
+import jp.co.di_system.android.omnidirectionalshooting.activity.base.BaseActivity;
 
-public class SettingActivity extends PreferenceActivity {
+public class SettingActivity extends BaseActivity implements View.OnClickListener {
     private final String TAG = "SettingActivity";
 
-    // 効果音
-    boolean isEffect;
+    // 攻撃音
+    boolean _isEffect;
     // BGM
-    boolean isBGM;
+    boolean _isBGM;
 
     Button effectButton;
     Button bgmBtn;
     Button backBtn;
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // 画面起動時に行う処理を記述
+    /**
+     * ここでXMLとクラスを緋も付ける
+     */
+    protected void retainXML() {
+        setContentView(R.layout.activity_setting);
+        effectButton = (Button) findViewById(R.id.effectButton);
+        bgmBtn = (Button) findViewById(R.id.bgmBtn);
+        backBtn = (Button) findViewById(R.id.backBtn);
+        effectButton.setOnClickListener(this);
+        bgmBtn.setOnClickListener(this);
+        backBtn.setOnClickListener(this);
+
+        this.setButtonText();
     }
 
+    public void setButtonText() {
+        this.setIsEffect(getIsEffect());
+        this.setIsBGM(getIsBGM());
+    }
+    @Override
+    /**
+     *ボタン押下時の処理
+     */
     public void onClick(View v) {
         // ボタン押下時の処理を記述
         if(v == effectButton) {
@@ -44,21 +63,67 @@ public class SettingActivity extends PreferenceActivity {
             Log.e(TAG, "onClickError");
         }
     }
+    public void onClickEffectSwitchButton () {
+        this.setIsEffect(!_isEffect);
+        effectButton.setSelected(_isEffect);
 
-
-    void onClickEffectSwitchButton () {
         Log.d(TAG, "onClickEffectSwitchButton");
     }
 
-    void onClickBgmSwitchButton () {
+    public void onClickBgmSwitchButton () {
+        this.setIsBGM(!_isBGM);
+        bgmBtn.setSelected(_isBGM);
+
         Log.d(TAG, "onClickBgmSwitchButton");
     }
 
-    void onClickBackButton () {
+    public void onClickBackButton () {
         Log.d(TAG, "onClickBackButton");
+        finish();
     }
 
+    public void setIsEffect (boolean isEffect) {
+        _isEffect = isEffect;
+        SharedPreferences prefs = getSharedPreferences(TAG, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("isEffect", isEffect);
+        editor.apply();
 
+        if (_isEffect) {
+            effectButton.setText(" 攻撃音：ON ");
+        } else {
+            effectButton.setText("攻撃音：OFF");
+        }
+
+    }
+
+    public void setIsBGM (boolean isBGM) {
+        _isBGM = isBGM;
+        SharedPreferences prefs = getSharedPreferences(TAG, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("isBGM",isBGM);
+        editor.apply();
+
+        if (_isBGM) {
+            bgmBtn.setText("BGM：ON ");
+        } else {
+            bgmBtn.setText("BGM：OFF");
+        }
+
+    }
+
+    public boolean getIsEffect () {
+        SharedPreferences prefs = getSharedPreferences(TAG, Context.MODE_PRIVATE);
+
+        return prefs.getBoolean("isEffect", false);
+    }
+
+    public boolean getIsBGM () {
+        SharedPreferences prefs = getSharedPreferences(TAG, Context.MODE_PRIVATE);
+
+        return prefs.getBoolean("isBGM", false);
+    }
 
 }
+
 
